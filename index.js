5471e0aee3ac9415c0fb5653eeb5a7d3b8e70df4
divya-kaushikkk <divyakaushik0117@example.com>
2025-09-19 18:39:20 +0530

    "update"

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from "express";
-import fetch from "node-fetch";
+import axios from "axios";
+import dotenv from "dotenv";
 
 
 const app = express();
 app.use(express.json());
-const TMDB_KEY = "my_tmdb_key";
+dotenv.config();
+// const TMDB_KEY = process.env.TMDB_KEY;
 
 app.get("/search", async (req, res) => {
     try{
         const movie = req.query.movie;
-        const movieApi = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${TMDB_KEY}&query=${movie}`);
-        const data = await movieApi.json();
-        return res.status(200).json({success: true, message: data});
+        const movieApi = await axios.get("https://api.themoviedb.org/3/search/multi",{
+            params: {
+                api_key: process.env.TMDB_KEY,
+                query: movie
+            }
+        }
+        );
+        return res.status(200).json({success: true, message: movieApi.data});
     }
     catch(error){
         console.log(error);
